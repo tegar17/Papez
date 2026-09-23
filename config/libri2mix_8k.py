d79f4055db9e490d2d35253b6e9ccfe00b8e5985
tegar17 <tegarprasetyo9f@gmail.com>
2026-09-23 21:38:30 +0700

    fix path

--- config/libri2mix_8k.py
+++ config/libri2mix_8k.py
@@ -1,6 +1,6 @@
 from core.moduledict import ModuleDict, Munch
 from core.dataset import LibriMix
-rootdir = "/data1/ohsai/LibriMix/"
+rootdir = "/kaggle/input/libri2mix-8khz-min"
 num_speakers = 2
 sample_rate = 8000
 config = Munch(
